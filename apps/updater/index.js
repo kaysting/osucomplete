e301723412e8f84bd585509a2585fc15d0aeea84
Kayla Kersting <kayla@kaysting.dev>
2026-06-08 00:52:49 -0500

    Check osu status every minute

--- apps/updater/index.js
+++ apps/updater/index.js
@@ -30,7 +30,7 @@ const checkOsuAccessibility = async () => {
             );
         }
     }
-    setTimeout(checkOsuAccessibility, 1000 * 60 * 10);
+    setTimeout(checkOsuAccessibility, 1000 * 60);
     return isOsuOnline;
 };
 
